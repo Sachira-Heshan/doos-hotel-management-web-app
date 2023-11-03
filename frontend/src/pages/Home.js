1d1b9647383b2a6014337ac995e16ee70329fe4d
Anuka Mithara Karunanayaka <anukamithara@gmail.com>
2023-11-03 13:26:04 +0530

    Fornt end

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,29 @@
 import React from 'react';
-
 import { Box } from '@chakra-ui/react';
+
+import {
+    BsFillCaretRightFill
+} from "react-icons/bs";import './Style.css'
 
 const Home = () => {
     return (
         <Box minW={'100%'} centerContent>
             <Box maxW={'1300px'} mx={'auto'}>
-                <div>Dashboard</div>
+                <div className='main-title'>
+                    <h3>DASHBOARD</h3>
+                </div>
+
+                <div className='main-cards'>
+                    <div className='card'>
+                        <div className='card-inner'>
+                            <h3>Available Rooms</h3>
+                        </div>
+                        <h1>300</h1>
+                    </div>
+
+                </div>
+
+
             </Box>
         </Box>
     );
--- frontend/src/pages/Reservation.js
+++ frontend/src/pages/Reservation.js
@@ -2,11 +2,47 @@ import React from 'react';
 
 import { Box } from '@chakra-ui/react';
 
+import {
+    BsFillCaretRightFill
+} from "react-icons/bs";import './Style.css'
+
 const Reservation = () => {
     return (
         <Box minW={'100%'} centerContent>
             <Box maxW={'1300px'} mx={'auto'}>
-                <div>Reservation</div>
+                <div className="main-title">
+                    <h3>Reservation</h3>
+                </div>
+
+                <div className="main-cards">
+                    <div className="card">
+                        <div className="card-inner">
+                            <h3>Create New Reservation</h3>
+                            <BsFillCaretRightFill className="card_icon" />
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <div className="card-inner">
+                            <h3>Show Reservation List</h3>
+                            <BsFillCaretRightFill className="card_icon" />
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <div className="card-inner">
+                            <h3>Edit Reservations</h3>
+                            <BsFillCaretRightFill className="card_icon" />
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <div className="card-inner">
+                            <h3>Cancel Reservations</h3>
+                            <BsFillCaretRightFill className="card_icon" />
+                        </div>
+                    </div>
+                </div>
             </Box>
         </Box>
     );
