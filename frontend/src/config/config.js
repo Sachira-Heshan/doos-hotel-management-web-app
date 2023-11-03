5957bd138cc5203a5b6d6b3ae893a95b94aaf503
Sachira Heshan <sachiraheshan99@gmail.com>
2023-11-03 13:28:10 +0530

    Frontend config

---
+++ frontend/src/config/config.js
@@ -0,0 +1,5 @@
+const BASE_URL = "http://172.31.98.26:3001/" //
+// const BASE_URL = "http://10.102.16.179:5001/" //uni wifi
+// const BASE_URL = "http://192.168.1.3:5001/" //yasiru wifi
+
+export { BASE_URL }
