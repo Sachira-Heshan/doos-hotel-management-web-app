900a2de9b1e16781c32fae971a4ee2a5b2c53d77
Anuka Mithara Karunanayaka <anukamithara@gmail.com>
2023-11-03 14:21:01 +0530

    login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -7,17 +7,17 @@ const Login = () => {
     return (
         <Container minW={'100%'} minH={'100vh'} centerContent>
             <Container maxW={'lg'} mt={12} centerContent>
-                <Box p={6} borderRadius={8} w={'sm'} maxW={'sm'} bg={'#ffb300'}>
+                <Box p={6} borderRadius={8} w={'sm'} maxW={'sm'} bg={'#2962ff'}>
                     <VStack>
-                        <LockIcon w={6} h={6} />
-                        <Text fontSize={'lg'} fontWeight='bold'>Login</Text>
+                        <LockIcon w={6} h={6} color={'#ffffff'}/>
+                        <Text fontSize={'lg'} fontWeight='bold' color={'#ffffff'}>Login</Text>
                         <form style={{ width: '100%' }}>
-                            <FormControl mt={4}>
+                            <FormControl mt={4} color={'#ffffff'}>
                                 <FormLabel>Email</FormLabel>
                                 <Input type='email' />
                                 <FormErrorMessage></FormErrorMessage>
                             </FormControl>
-                            <FormControl mt={4}>
+                            <FormControl mt={4} color={'#ffffff'}>
                                 <FormLabel>Password</FormLabel>
                                 <Input type='password' />
                             </FormControl>
